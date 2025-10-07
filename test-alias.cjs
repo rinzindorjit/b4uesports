36ec18f0e2612002e157c5f16684575fcebaaee6
rinzindorjit <iyus.rigzin901@gmail.com>
2025-10-07 08:59:24 -0700

    Remove temporary files

--- test-alias.cjs
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-console.log('Current directory:', __dirname);
-console.log('Resolved @ alias:', path.resolve(__dirname, "client", "src"));
-console.log('Toaster path:', path.resolve(__dirname, "client", "src", "components", "ui", "toaster.tsx"));
