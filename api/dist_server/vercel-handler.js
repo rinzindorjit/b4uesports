6fb82a1c5a1c1bd4b87ddd2ec87a70aa999ec4ce
rinzindorjit <iyus.rigzin901@gmail.com>
2025-10-08 02:10:52 -0700

    Fix Vercel function limit by consolidating API functions and adding .vercelignore

--- api/dist_server/vercel-handler.js
+++ api/dist_server/vercel-handler.js
@@ -13,20 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 })();
 const handler = serverless(app);
 var vercel_handler_default = handler;
-import { default as default2 } from "../api/users.js";
-import { default as default3 } from "../api/packages.js";
-import { default as default4 } from "../api/pi-price.js";
-import { default as default5 } from "../api/payments.js";
-import { default as default6 } from "../api/transactions.js";
-import { default as default7 } from "../api/admin.js";
-import { default as default8 } from "../api/test.js";
 export {
-  default7 as admin,
-  vercel_handler_default as default,
-  default3 as packages,
-  default5 as payments,
-  default4 as piPrice,
-  default8 as test,
-  default6 as transactions,
-  default2 as users
+  vercel_handler_default as default
 };
--- server/vercel-handler.ts
+++ server/vercel-handler.ts
@@ -21,14 +21,3 @@ const handler = serverless(app);
 
 // Export the handler for Vercel
 export default handler;
-
-// Export individual API endpoints for direct access
-export { default as users } from '../api/users';
-export { default as packages } from '../api/packages';
-export { default as piPrice } from '../api/pi-price';
-export { default as payments } from '../api/payments';
-export { default as transactions } from '../api/transactions';
-export { default as admin } from '../api/admin';
-export { default as test } from '../api/test';
-
-// Force rebuild by adding a comment
