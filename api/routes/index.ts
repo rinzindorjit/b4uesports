5b74c236388ab9a961935d9b4b7148ee9d073756
rinzindorjit <iyus.rigzin901@gmail.com>
2025-10-11 00:12:10 -0700

    Remove vercel-handler and simplify API structure to use individual serverless functions

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,5 +1,2 @@
 // Re-export all route handlers from individual files
 export { default as piPriceRouter } from './pi-price.js';
-
-// Export the registerRoutes function from the main routes file
-export { registerRoutes } from '../routes.js';
